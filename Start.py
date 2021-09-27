9efa99364b8d7167d3a81daae7c930042ce4290c
64500 <2506189230@qq.com>
2021-09-27 18:01:38 +0800

    绘制GUI
    完善了游戏进行界面的GUI

--- Start.py
+++ Start.py
@@ -2,6 +2,7 @@ import wx
 import Gamer
 import yaml
 import os
+import random
 
 
 def get_yaml_data_all(yaml_file):
@@ -17,6 +18,15 @@ def get_yaml_data_all(yaml_file):
 # get_yaml_data_all(yaml_path)
 # 使用get_yaml_data_all函数的示例。
 
+Events = ['你今天等公交车的时候，发现天桥下跳下来一个赛里斯女超人，可惜她摔死了，你大为震撼。',
+          '你刷手机的时候，看见了一条恐婚新闻，你感觉很惊恐，离爱情又远了很多。(焦虑+4)',
+          '你发现了一个黑人留居时间过长，于是举报了他，发现是一个偷渡佬，公安局奖励了你。（资源+3）',
+          '你差点被泥头车创死，但旁边被撞到的饮料售卖机凭空消失了，你十分后悔。（焦虑+10）',
+          '你今天打了一把Moba游戏，打完后发现自己不用吃饭了（本回合不扣除生活资源）',
+          '你有那个大病，于是去人民医院看了看，医生给你看了你的大病。(健康+4，焦虑-10,资源-1)',
+          '你看见一辆轿车上画着膏药旗，感到大为震撼。'
+          ]
+
 
 class GameWindow(wx.Frame):
     # 定义游戏窗口
@@ -26,11 +36,48 @@ class GameWindow(wx.Frame):
 
         self.time = 1
         self.run = 5
+        self.Health_data = -18
+        self.anxious = 144
+        self.intelligence_data = 4
+        self.resources_data = 5
+
         self.title = wx.StaticText(panel2, label='回合：'+str(self.time), pos=(60, 10))
+        self.title2 = wx.StaticText(panel2, label='身份：拳师', pos=(60, 60))
+        self.TYTianFu_1 = wx.Button(panel2, label='通用天赋：蓄意轰拳', pos=(60, 400))
+        self.TYTianFu_2 = wx.Button(panel2, label='通用天赋：祖安艺术', pos=(60, 430))
+        self.TYTianFu_3 = wx.Button(panel2, label='通用天赋：量子速读', pos=(60, 460))
+        self.SFTianFu = wx.Button(panel2, label='身份天赋：CIA兼职', pos=(60, 510))
+        self.Ues_Card = wx.Button(panel2, label='打出卡牌', pos=(60, 540))
         font = wx.Font(24, wx.DEFAULT, wx.FONTSTYLE_NORMAL, wx.NORMAL)
         self.title.SetFont(font)
 
+        self.Health = wx.StaticText(panel2, label='健康值:' + str(self.Health_data), pos=(320, 40))
+        self.Anxious = wx.StaticText(panel2, label='焦虑值:' + str(self.anxious), pos=(320, 70))
+        self.intelligence = wx.StaticText(panel2, label='能力值:' + str(self.intelligence_data), pos=(400, 40))
+        self.resources = wx.StaticText(panel2, label='资源点:' + str(self.resources_data), pos=(400, 70))
+
+        self.Gender = wx.StaticText(panel2, label='性别：女', pos=(460, 40))
+        self.height = wx.StaticText(panel2, label='身高：150 CM', pos=(460, 60))
+        self.ZongHe = wx.StaticText(panel2, label='综合：白毛萝莉拳师', pos=(460, 80))
+
+        self.Card1 = wx.StaticText(panel2, label='手牌：马恩牌X1', pos=(200, 410))
+        self.Card2 = wx.StaticText(panel2, label='手牌：喷射牌X1', pos=(200, 440))
+        self.Card3 = wx.StaticText(panel2, label='手牌：马恩牌X1', pos=(200, 470))
+        self.Card4 = wx.StaticText(panel2, label='手牌：马科长X1', pos=(200, 500))
+        self.Card5 = wx.StaticText(panel2, label='手牌：锡特勒X1', pos=(200, 530))
+        self.Card6 = wx.StaticText(panel2, label='手牌：贵左X1', pos=(200, 560))
+
+        self.rb1 = wx.RadioButton(panel2, -1, label='1', pos=(320, 410), style=wx.RB_GROUP)
+        self.rb2 = wx.RadioButton(panel2, -1, label='2', pos=(320, 440))
+        self.rb3 = wx.RadioButton(panel2, -1, label='3', pos=(320, 470))
+        self.rb4 = wx.RadioButton(panel2, -1, label='4', pos=(320, 500))
+        self.rb5 = wx.RadioButton(panel2, -1, label='5', pos=(320, 530))
+        self.rb6 = wx.RadioButton(panel2, -1, label='6', pos=(320, 560))
+
         self.next = wx.Button(panel2, -1, label='下一回合', pos=(490, 540))
+        self.Get_Card = wx.Button(panel2, -1, label='抽取卡片', pos=(490, 500))
+
+        self.Buff = wx.TextCtrl(panel2, -1, value='buff栏', pos=(470, 100), size=(100, 300), style=wx.TE_MULTILINE)
 
         self.Bind(wx.EVT_BUTTON, self.Next_continue, self.next)
 
@@ -51,7 +98,7 @@ class GameWindow(wx.Frame):
 运转到了极限，人们纷纷追忆起三年前萨布林娜小姐在全国广
 播里的讲话，高声唱起图书馆之歌来，似乎是可以念一首《海
 燕》了。'''
-                                           , pos=(50, 100), size=(360, 300), style=wx.TE_MULTILINE)
+                                           , pos=(50, 100), size=(400, 300), style=wx.TE_MULTILINE)
 
     def Next_continue(self, event):
         self.time = self.time + 1
@@ -59,6 +106,7 @@ class GameWindow(wx.Frame):
         self.title.SetLabel('回合：'+str(self.time))
         self.run_title.SetLabel('剩余行动力: ' + str(self.run))
         self.mess.AppendText('\n测试事件：你早上看见了女人打拳，顿生惊恐（焦虑值+8）')
+        self.mess.AppendText('\n' + Events[int(6*random.random())+1])
 
     def Action(self, event):
         self.run = self.run - 1
@@ -77,6 +125,7 @@ class GameWindow(wx.Frame):
             if errorMessage.ShowModal() == wx.ID_YES:
                 errorMessage.Destroy()
         self.mess.AppendText('\n事件：你认真学习，提高了自己的能力！（能力+1）')
+
     def TianFu_Action(self, event):
         self.run = self.run - 1
         self.run_title.SetLabel('剩余行动力: ' + str(self.run))
