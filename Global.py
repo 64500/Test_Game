bad70a6f4660e5d30fd40cec2f52bc12906c858a
バターネコ <buttercat_hym@outlook.com>
2021-10-01 17:35:11 +0800

    配置文件格式修正

--- Global.py
+++ Global.py
@@ -1,3 +1,6 @@
+# HYM740
+# 单例模式装饰器
+# https://zhuanlan.zhihu.com/p/37534850
 class Singleton(object):
     def __init__(self, cls):
         self._cls = cls
@@ -11,5 +14,14 @@ class Singleton(object):
 # 此类使用单例模式，只允许存在一个此类的对象
 @Singleton
 class Global:
-    def __init__(self) -> None:
+    def __init__(self):
+        self.time=0;
         pass
+    @property
+    def time(self):
+        return self.time
+    @time.setter
+    def time(self,value):
+        self.time+=value
+    def next(self):
+        self.time+=1;
